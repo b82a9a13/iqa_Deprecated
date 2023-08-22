b30947373c5384151f9f470067002fa4d8657622
b82a9a13 <85174237+b82a9a13@users.noreply.github.com>
2023-08-22 16:03:59 +0100

    v0.1.1

---
+++ classes/lib.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     local_iqa
+ * @author      Robert Tyrone Cullen
+ * @var stdClass $plugin
+ */
+namespace local_iqa;
+use stdClass;
+
+class lib{
+
+}
---
+++ db/access.php
@@ -0,0 +1,28 @@
+<?php
+$capabilities = [
+    'local/iqa:coach' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW
+        ]
+    ],
+    'local/iqa:learner' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'student' => CAP_ALLOW
+        ]
+    ],
+    'local/iqa:admin' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'manager' => CAP_ALLOW
+        ]
+    ]
+];
