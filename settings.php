5dc710aa3ce0adb52fbb56197e15e5856c20c6ee
b82a9a13 <85174237+b82a9a13@users.noreply.github.com>
2023-08-22 14:27:46 +0100

    v0.1.0

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* 
+* @package     local_iqa
+* @author      Robert Tyrone Cullen
+*/
+
+defined('MOODLE_INTERNAL') || die();
---
+++ version.php
@@ -0,0 +1,13 @@
+<?php
+// This file is part of the IQA Plugin
+/**
+ * @package     local_iqa
+ * @author      Robert Tyrone Cullen
+ * @var stdClass $plugin
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_iqa';
+$plugin->version = 1;
+$plugin->requires = 2016052314; // Moodle version
